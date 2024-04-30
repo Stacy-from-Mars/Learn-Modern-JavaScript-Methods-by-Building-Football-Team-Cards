f23fb3a8952f6212bcfe8be2bd9c1c272c4c9b7e
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-04-30 22:55:50 +0300

    step 36

--- script.js
+++ script.js
@@ -196,4 +196,4 @@ const setPlayerCards = (arr = players) => {
     ).join("");
 }
 
-playersDropdownList.addEventListener("change", () => {});
+playersDropdownList.addEventListener("change", (e) => {});
